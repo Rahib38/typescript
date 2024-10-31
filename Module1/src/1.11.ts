4649d7376885f27946b3dd7ba532d1c068c50c30
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-10-31 09:11:53 -0700

    added

--- Module1/src/1.11.ts
+++ Module1/src/1.11.ts
@@ -7,6 +7,37 @@
     console.log("not adult");
   }
 
+  //   ternary
+
   const idAdult = age >= 18 ? "Adult" : "Not adult";
   console.log(idAdult);
+
+  //   nullish
+
+  const isAuthenticate = "";
+  const result1 = isAuthenticate ?? "Guest";
+  const result2 = isAuthenticate ? isAuthenticate : "Guest";
+  console.log({ result1 }, { result2 });
+
+  type User = {
+    name: string;
+    address: {
+      city: string;
+      road: string;
+      presentaddress: string;
+      premanentAddress?: string;
+    };
+  };
+
+  const user: User = {
+    name: "Nadim",
+    address: {
+      city: "Feni",
+      road: "Awesome road",
+      presentaddress: "Feni town",
+    },
+  };
+  const premanentAddress =
+    user?.address?.premanentAddress ?? "No permanetAddress";
+  console.log(premanentAddress);
 }
