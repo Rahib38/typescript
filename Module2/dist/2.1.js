f7fb153baed24d6efaf7e8da7c6b8276966b8381
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-11-02 18:54:53 -0700

    added

---
+++ Module2/dist/2.1.js
@@ -0,0 +1,18 @@
+"use strict";
+{
+    var anything = void 0;
+    anything = "next level web development";
+    anything = 225;
+    anything;
+    var kgToGm = function (value) {
+        if (typeof value === "string") {
+            var convertedValue = parseFloat(value) * 1000;
+            return "The converted value is : ".concat(convertedValue);
+        }
+        if (typeof value === "number") {
+            return value * 1000;
+        }
+    };
+    var result1 = kgToGm(1000);
+    console.log(result1);
+}
