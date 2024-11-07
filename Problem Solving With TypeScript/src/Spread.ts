60d54f5e618bd80d229f9e2dd13f7097683c9659
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-11-06 22:16:04 -0800

    added

---
+++ Problem Solving With TypeScript/src/Spread.ts
@@ -0,0 +1,36 @@
+{
+  function arg(...sum: number[]): number {
+    return sum.reduce((acc, curr) => acc + curr, 10);
+  }
+  console.log(
+    arg(
+      1,
+      0,
+      5,
+      48,
+      4,
+      2,
+      781,
+      5,
+      81,
+      71,
+      81,
+      47814,
+      8,
+      58,
+      8,
+      58,
+      8,
+      5,
+      8,
+      578,
+      5,
+      84,
+      58,
+      215,
+      8,
+      4,
+      88
+    )
+  );
+}
